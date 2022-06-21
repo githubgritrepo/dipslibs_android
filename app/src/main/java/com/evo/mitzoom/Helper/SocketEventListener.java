6e46010e9f2a68751a3a24822a07a2325a086787
mutaqin.02 <mutaqin.02@gmail.com>
2022-06-21 14:29:28 +0700

    socketbackground

---
+++ app/src/main/java/com/evo/mitzoom/Helper/SocketEventListener.java
@@ -0,0 +1,25 @@
+package com.evo.mitzoom.Helper;
+
+import io.socket.emitter.Emitter;
+
+public class SocketEventListener extends Emitter implements Emitter.Listener {
+
+    private String mEvent;
+    private Listener mListener;
+
+    public SocketEventListener(String mEvent, Listener mListener) {
+        this.mEvent = mEvent;
+        this.mListener = mListener;
+    }
+
+    @Override
+    public void call(Object... objects) {
+        if (this.mListener != null) {
+            this.mListener.onEventCall(mEvent, objects);
+        }
+    }
+
+    public interface Listener {
+        void onEventCall(String event, Object... objects);
+    }
+}
